e0b39cca148f97043beba73aaee4fa09ed0519df
369webforall <369webforall@gmail.com>
2025-06-17 17:59:46 +0545

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ export default function Page() {
     <>
       <h1>App Router</h1>
       <h2>This is main page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
+        dignissimos placeat harum atque. Consectetur, sed veritatis consequuntur
+        cupiditate omnis tempore accusamus animi commodi voluptatum debitis.
+        Qui, deleniti exercitationem. Illo, quod.
+      </p>
     </>
   );
 }
